a94ee9093482b5f8cc1283ea8fd46070a68edb5d
Mudassar Ali <mudassar.ali@vortx.com>
2024-01-28 23:59:40 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SealedClassesInCSharp;
+
+SealedClass sealedClass = new SealedClass();
+Console.WriteLine(sealedClass.Add());
---
+++ SealedClassesInCSharp.cs
@@ -0,0 +1,40 @@
+﻿namespace SealedClassesInCSharp
+{
+    //Implementing sealed class
+    sealed class SealedClass
+    {
+        public int x;
+        public int y;
+
+        public SealedClass()
+        {
+            x = 100;
+            y = 200;
+        }
+
+        public int Add()
+        {
+            return x + y;
+        }
+
+        //subtract
+        public int Sub()
+        {
+            return x - y;
+        }
+
+        //Multiply
+        public int Mul()
+        {
+            return x * y;
+        }
+
+        //Divide
+        public int Div()
+        {
+            return x / y;
+        }
+    }
+
+}
+
